8746c7b99de8379b5ad7abc980b955b62d6ce899
Rick Miyamoto <rkenmi@gmail.com>
2017-10-28 21:50:46 -0700

    add react-bootstrap

--- static/src/App.js
+++ static/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Button, ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    let loginUrl = `/api`;
     return (
       <div className="App">
         <header className="App-header">
@@ -12,9 +14,18 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          Or if you're using webpack-dev-server, no need to reload.
 
           ( ͡° ͜ʖ ͡°)
         </p>
+        <ButtonGroup>
+          <DropdownButton id="dropdown-btn-menu" bsStyle="success" title="Dropdown">
+            <MenuItem key="1">Dropdown link</MenuItem>
+            <MenuItem key="2">Dropdown link</MenuItem>
+          </DropdownButton>
+          <Button bsStyle="info">Middle</Button>
+          <Button bsStyle="info">Right</Button>
+        </ButtonGroup>
       </div>
     );
   }
--- static/src/index.js
+++ static/src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { AppContainer } from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader';
+import navbarInstance from './Navbar';
 
 const render = Component => {
-    ReactDOM.render(
-      <AppContainer>
-          <Component />
-      </AppContainer>,
-      document.getElementById('root'),
-    );
-    registerServiceWorker();
-}
+  ReactDOM.render(navbarInstance, document.getElementById('topnav'));
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+  registerServiceWorker();
+};
 
-render(App)
+render(App);
 
 // Make components hot reloadable
 if (module.hot) {
   module.hot.accept('./App', () => { render(App) })
+  module.hot.accept('./Navbar', () => { render(App) })
 }
