33cafe5ef105301b9e44d3bf3ac97dfd77f0cec2
Rick Miyamoto <rkenmi@gmail.com>
2019-07-02 22:32:54 -0700

    add more testing for redux

--- static/src/__tests__/reducers.test.js
+++ static/src/__tests__/reducers.test.js
@@ -1,5 +1,7 @@
 import {loginFailure, setAuthToken} from "../actions/authActions";
 import authReducer from "../reducers/authReducer";
+import {filteredBPMResponse, getGenresResponse} from "../actions/searchActions";
+import searchReducer from "../reducers/searchReducer";
 
 describe('auth reducer', () => {
  it('saves error message from login', () => {
@@ -25,4 +27,37 @@ describe('auth reducer', () => {
     const result = authReducer(state, action);
     expect(result.token).toEqual('test token');
   })
+});
+
+describe('search reducer', () => {
+  const state = {
+    results: [],
+    page: 0,
+    totalPages: 0,
+    queryResponseMs: 0,
+    allGenres: [],
+  };
+
+  let action = filteredBPMResponse([1, 2, 3], 1, 1000);
+  let result = searchReducer({...state}, action);
+
+  it('returns filtered search results', () => {
+    expect(result.results).toEqual([1, 2, 3]);
+  });
+
+  it('returns the correct number of total pages', () => {
+    expect(result.totalPages).toEqual(1);
+  });
+
+  it('returns the time taken for the query response', () => {
+    expect(result.queryResponseMs).toEqual(1000);
+  });
+
+  action = getGenresResponse(['Punk', 'Harmonica']);
+  result = searchReducer({...result}, action);
+
+  it('returns the available genres that are selectable for filtering', () => {
+    expect(result.allGenres).toEqual(['Punk', 'Harmonica']);
+  });
+
 });
